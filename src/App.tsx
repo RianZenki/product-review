7aac9b8667c235ec2e4a575cad2531c610d73154
Rian Zenki Nacazato <rian.zenki@gmail.com>
2023-10-03 19:18:51 -0300

    add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import './App.css'
+import { Button } from './components/ui/Button'
+import { StarRating } from './components/StarRating';
+
+import classes from "./App.module.css";
+
+import smartwatch from './assets/watch.png'
 
 export function App() {
+	return (
+		<div className={classes.container}>
+			<div className={classes.card}>
+        <header>
+            <img src={smartwatch} alt="smartwatch amazfit" />
+        </header>
+        <main className={classes.content}>
+          <div className={classes.title}>
+            <h2>Avalie o produto</h2>
+            <p>O que você achou do produto <strong>Smartwatch Amazfit Bip U Pro</strong>?</p>
+          </div>
 
-  return (
-    <>
-      <p>Olá mundo</p>
-    </>
-  )
+          <StarRating />
+
+          <Button className={classes.confirm}>Confirmar</Button>
+        </main>
+      </div>
+		</div>
+	);
 }
- 
-
