c59a9e24bd285701cfd7b8920b423ea0882a1ba0
Rian Zenki Nacazato <rian.zenki@gmail.com>
2023-10-04 12:30:03 -0300

    change star rating component

--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
+
 import classes from "./StarRating.module.css";
 
 export const StarRating = () => {
+	const [rating, setRating] = useState<number | null>(null);
+
+	const ratingValues = [4, 3, 2, 1, 0];
+
+	const handleChangingValue = (ratingValue: number) => {
+		setRating(ratingValue);
+	};
+
 	return (
 		<div className={classes["rating-container"]}>
 			<div className={classes["star-wrapper"]}>
-				<span>&#9733;</span>
-				<span>&#9733;</span>
-				<span>&#9733;</span>
-				<span>&#9733;</span>
-				<span>&#9733;</span>
+				{ratingValues.map((ratingValue) => (
+					<span
+						onClick={() => handleChangingValue(ratingValue)}
+						className={ratingValue === rating ? classes.selected : ""}
+					>
+						&#9733;
+					</span>
+				))}
 			</div>
 			<div className={classes.description}>
 				<p>Ruim</p>
