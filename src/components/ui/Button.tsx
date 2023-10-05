a35a316d2e49839ce8396441361855049e770958
Rian Zenki Nacazato <rian.zenki@gmail.com>
2023-10-05 16:13:38 -0300

    add disabled button when rating star not checked

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -5,11 +5,15 @@ import classes from "./Button.module.css";
 interface ButtonProps {
 	children: string;
 	className?: string;
+	disabled: boolean
 }
 
 export const Button = (props: ButtonProps) => {
 	return (
-		<button className={`${classes.button} ${props.className}`}>
+		<button
+			className={`${classes.button} ${props.className}`}
+			disabled={props.disabled}
+		>
 			{props.children}
 			<ArrowRight size={24} />
 		</button>
